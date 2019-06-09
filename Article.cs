e4c04b3994076ef291f4dd863f90635ce77a8711
Abhiodinson <45389282+Abhiodinson@users.noreply.github.com>
2019-06-10 00:47:08 +0200

    Create Article.cs

---
+++ Article.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Wpf_AntiqueShop
+{
+    public class ArticleName
+    {
+        public String id { get; set; }
+        public String name { get; set; }
+        public String description { get; set; }
+        public String price { get; set; }
+
+
+    }
+}
