c377a20f700cfc12f8fd75573d527c41e0952e31
Abhiodinson <45389282+Abhiodinson@users.noreply.github.com>
2019-06-13 01:39:35 +0200

    Create ArticleInfo.cs

---
+++ ArticleInfo.cs
@@ -0,0 +1,12 @@
+namespace Wpf_AntiqueShop
+{
+    public class ArticleInfo
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string soldarticleName { get; set; }
+        public string purchasedarticleName { get; set; }
+        public string price { get; set; }
+
+    }
+}
