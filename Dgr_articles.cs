dd140aa778899f3559e6e85117f674d4c8a77f20
Abhiodinson <45389282+Abhiodinson@users.noreply.github.com>
2019-06-13 01:40:40 +0200

    Create Dgr_articles.cs

---
+++ Dgr_articles.cs
@@ -0,0 +1,15 @@
+namespace Wpf_AntiqueShop
+{
+    internal class Dgr_articles
+    {
+        public Dgr_articles(string name)
+        {
+            this.name = name;
+        }
+
+        public string id { get; set; }
+        public string name { get; set; }
+        public string price { get; set; }
+
+    }
+}
