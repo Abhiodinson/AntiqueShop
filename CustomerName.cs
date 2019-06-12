fb233caa27ef7f5c7bee5f2d8bbb8fb3dc61aedb
Abhiodinson <45389282+Abhiodinson@users.noreply.github.com>
2019-06-13 01:41:13 +0200

    Create CustomerName.cs

---
+++ CustomerName.cs
@@ -0,0 +1,14 @@
+namespace Wpf_AntiqueShop
+{
+    public class CustomerName
+    {
+        
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+
+    }
+}
